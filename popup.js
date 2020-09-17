25775f1d342ecccb9208d77279a21de3a7cb330b
AnthonyClausing <AnthonyClausing@gmail.com>
2020-09-17 03:35:17 -0400

    changed some styling and text tabs now saved through chunking removed a few comments

--- popup.js
+++ popup.js
@@ -1,18 +1,12 @@
-//get ul element of list
 let listDiv = document.getElementById("locker-container");
 let saveSlots = [1,2,3,4,5]
+const SECTION_LIMIT = 15
 
-///////WHY RESET WHOLE THING
-//////WHY RESET WHOLE THING
-///WHY RESET WHOLE THING
-///////WHY RESET WHOLE THING
-//////WHY RESET WHOLE THING
-///////WHY RESET WHOLE THING
-////LIMIT ON INPUT 22 characters?
-///WHEN "LOCKING" make sure MORE THAN visually 'CLOSED"
+// ///WHEN "LOCKING" make sure MORE THAN visually 'CLOSED"
 function makeTwoElements(elem) {
   return [document.createElement(elem),document.createElement(elem)]
 }
+
 function clearClassList(elem) {
   var classList = elem.classList;
   while (classList.length > 0) {
@@ -20,38 +14,50 @@ function clearClassList(elem) {
   }
   return elem
 }
+
 function createLocker(slot) {
   let inputInfo = document.getElementById("name-" + slot)
   let date = new Date()
   let period = date.getHours() >= 12 ? "PM" : "AM"
   let [month,day,year,hours,mins] = [date.getMonth() + 1, date.getDate(), date.getFullYear(), Math.ceil((date.getHours()+ 1)/2), date.getMinutes()]
-  let formattedDate = `${month}/${day}/${year} ${hours}:${mins} ${period}`
+  let formattedDate = `${month}/${day}/${year} ${hours}:${mins < 10 ? '0' + mins : mins} ${period}`
 
-  chrome.storage.sync.get("sb_lockers",function({sb_lockers}) { //get the data
-    chrome.tabs.query({currentWindow: true}, tabs => { // get the tabs
-      let newStorage = { //normalize the tabs into payload object
-        ...sb_lockers,
+  chrome.storage.sync.get("tabLockers",function({tabLockers}) {
+    chrome.tabs.query({currentWindow: true}, tabs => {
+      let newTabs = tabs.map(({url,title,favIconUrl}) => {
+        return {url, title, favIconUrl}
+      })
+      let sections =  Math.ceil(newTabs.length/SECTION_LIMIT)
+      let newStorage = { 
+        ...tabLockers,
         [slot]: { 
           header: inputInfo.value || formattedDate, 
-          tabs: tabs.map(({url,title,favIconUrl}) => {
-            return {url, title, favIconUrl}
-          }) 
+          sections
         }
       }
-      chrome.storage.sync.set({sb_lockers:  newStorage}, resetView) //set the data on storage
+      chrome.storage.sync.set({tabLockers:  newStorage})
+      for(let i = 1; i <= sections;i++) {
+        chrome.storage.sync.set({[`tabLocker${slot}${i}`] : newTabs.slice(SECTION_LIMIT * (i-1), SECTION_LIMIT * i)})
+      }
+      resetLocker(slot, newTabs, newStorage[slot].header)
     })
   })
 }
 
 function removeLocker(slot) {
-  chrome.storage.sync.get("sb_lockers",function({sb_lockers}) { //get the data
-    let newStorage = { ...sb_lockers, [slot]: {}} //reassign slot
-    chrome.storage.sync.set({sb_lockers:  newStorage}, resetView) //set the data on storage
+  chrome.storage.sync.get("tabLockers",function({tabLockers}) {
+    let slotSections = tabLockers[slot].sections
+    let newStorage = { ...tabLockers, [slot]: {header: "", sections: 0}} 
+    chrome.storage.sync.set({tabLockers:  newStorage})
+    let sectionsToRemove = [...Array(slotSections)].map((e,index) => `tabLocker${slot}${index + 1}`)
+    chrome.storage.sync.remove(sectionsToRemove, resetLocker.bind(null, slot, []))
   })
 }
+
 function openWindow(tabs) {
   chrome.windows.create({url: tabs.map(tab => tab.url), state: "maximized"})
 }
+
 function resetButtons(saveButton, deleteButton, tabs, slot) {
   let newSaveBtn = clearClassList(saveButton.cloneNode(true))
   let newDeleteBtn = deleteButton.cloneNode(true)
@@ -65,6 +71,7 @@ function resetButtons(saveButton, deleteButton, tabs, slot) {
   saveButton.parentNode.replaceChild(newSaveBtn, saveButton);
   deleteButton.parentNode.replaceChild(newDeleteBtn, deleteButton);
 }
+
 function resetTopDiv(topDiv, tabLength, slot, header) {
   let inputInfo = document.getElementById("name-" + slot)
   let nameInput = document.createElement("input")      
@@ -74,60 +81,92 @@ function resetTopDiv(topDiv, tabLength, slot, header) {
   if(!tabLength) {
     nameInput.id = "name-" + slot
     nameInput.style.padding = "3px"
-    nameInput.placeholder = "Type Name Before Saving"
+    nameInput.style.textAlign = "center"
+    nameInput.placeholder = "INSERT NAME"
+    nameInput.maxLength = 21
     topDiv.appendChild(nameInput)
   }
 }
-function resetView(){
+
+function resetLocker(slot, tabs, header){
   ///ADD A LITTLE SOMETHING, ANIMATION???
-  chrome.storage.sync.get("sb_lockers", function({sb_lockers}) { 
-    let lockers = sb_lockers || {}
-    saveSlots.forEach((slot) => {
-      let listItem = document.getElementById("locker-" + slot)
-      let tabs = lockers[slot] && lockers[slot].tabs || []
-      let header = lockers[slot] && lockers[slot].header || ""
-      resetButtons(...listItem.children[1].children, tabs, slot)
-      resetTopDiv(listItem.children[0], tabs.length, slot, header)
-    })
+    let listItem = document.getElementById("locker-" + slot)
+    resetButtons(...listItem.children[1].children, tabs, slot)
+    resetTopDiv(listItem.children[0], tabs.length, slot, header)
+}
+
+function startUpView(tabsList, lockers) {
+  tabsList.forEach(function(tabs,index){
+    let slot = index + 1
+    let nameInput = document.createElement("input")
+    let listItem = document.createElement("li")
+    let [topDiv, botDiv] = makeTwoElements("div")
+    let [saveBtn,deleteBtn] = makeTwoElements("button")
+
+    nameInput.id = "name-" + slot
+    nameInput.style.padding = "3px"
+    nameInput.style.textAlign = "center"
+    nameInput.placeholder = "INSERT NAME"
+    nameInput.maxLength = 21
+    listItem.id = "locker-" + slot      
+    listItem.classList.add("lockers")
+    topDiv.classList.add("locker-name")
+    botDiv.style.display = "flex"
+    botDiv.style.display = "space between"
+    saveBtn.classList.add( tabs.length ? "open" : "save")    
+    saveBtn.innerText = tabs.length ? "Open " + tabs.length + " Tabs" : "Save Tabs"
+    saveBtn.addEventListener("click", tabs.length ? openWindow.bind(null, tabs) : createLocker.bind(null, slot))
+    deleteBtn.innerText = "Delete"
+    deleteBtn.disabled = !tabs.length
+    deleteBtn.classList.add("delete")
+    if(tabs.length) {
+      deleteBtn.addEventListener("click", removeLocker.bind(null, slot))
+      nameInput.display = "none"
+      topDiv.innerText = lockers[slot].header || tabs.length + " Tabs"
+    } else {
+      topDiv.appendChild(nameInput)
+    }
+    botDiv.appendChild(saveBtn)
+    botDiv.appendChild(deleteBtn)
+    listDiv.appendChild(listItem)
+    listItem.appendChild(topDiv)
+    listItem.appendChild(botDiv)
   })
 }
-function startUp() {
-  chrome.storage.sync.get("sb_lockers", function({sb_lockers}) { 
-    let lockers = sb_lockers || {}
-    saveSlots.forEach((slot) => {
-      let tabs = lockers[slot] && lockers[slot].tabs || []
-      let nameInput = document.createElement("input")
-      let listItem = document.createElement("li")
-      let [topDiv, botDiv] = makeTwoElements("div")
-      let [saveBtn,deleteBtn] = makeTwoElements("button")
 
-      nameInput.id = "name-" + slot
-      nameInput.style.padding = "3px"
-      nameInput.placeholder = "Type Name Before Saving"
-      listItem.id = "locker-" + slot      
-      listItem.classList.add("lockers")
-      topDiv.classList.add("locker-name")
-      botDiv.style.display = "flex"
-      botDiv.style.display = "space between"
-      saveBtn.classList.add( tabs.length ? "open" : "save")    
-      saveBtn.innerText = tabs.length ? "Open " + tabs.length + " Tabs" : "Save Tabs"
-      saveBtn.addEventListener("click", tabs.length ? openWindow.bind(null, tabs) : createLocker.bind(null, slot))
-      deleteBtn.innerText = "Delete"
-      deleteBtn.disabled = !tabs.length
-      deleteBtn.classList.add("delete")
-      if(tabs.length) {
-        deleteBtn.addEventListener("click", removeLocker.bind(null,slot))
-        nameInput.display = "none"
-        topDiv.innerText = lockers[slot].header || tabs.length + " Tabs"
-      } else {
-        topDiv.appendChild(nameInput)
-      }
-      botDiv.appendChild(saveBtn)
-      botDiv.appendChild(deleteBtn)
-      listDiv.appendChild(listItem)
-      listItem.appendChild(topDiv)
-      listItem.appendChild(botDiv)
-    })
+function startUpData(callback, lockers) {
+  let tabsList = []
+  saveSlots.forEach((slot, slotIndex) => {
+    let tabsForSlot = []
+    let sections = lockers[slot].sections
+    let i = 0;
+    do {
+      let tabsChunkTag = `tabLocker${slot}${i + 1}`
+      chrome.storage.sync.get(tabsChunkTag, function (result) {
+        if(!result[tabsChunkTag]) {
+          tabsList.push([])
+          if(slotIndex === 4) callback(tabsList, lockers)
+        } else {
+          tabsForSlot.push(...result[tabsChunkTag])
+          if(i + 2 >= sections) {
+            tabsList.push(tabsForSlot)
+            if(slotIndex === 4) {
+              callback(tabsList, lockers)
+             } 
+          }
+        }
+      })
+      i = i + 1;
+    } while (i + 1 < sections);
+  })
+}
+
+function startUp() {
+  //call this on download?v?v?v?v?v?v?
+  // chrome.storage.sync.set({tabLockers:  {1: {header: '', sections: 0}, 2:{header:'', sections: 0}, 3: {header:'', sections:0}, 4:{header:'', sections:0}, 5:{header:'', sections:0}}})
+  chrome.storage.sync.get("tabLockers", function({tabLockers}) { 
+    let lockers = tabLockers || {1: {header: '', sections: 0}, 2:{header:'', sections: 0}, 3: {header:'', sections:0}, 4:{header:'', sections:0}, 5:{header:'', sections:0}}
+    startUpData(startUpView, lockers)
   })
 }
 startUp()
